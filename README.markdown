15cb1143c824336481496816584e1a6a757a0250
SomeRandomNerd <somerandomnerd@gmail.com>
2011-03-07 21:54:04 +0000

    INitial commit

---
+++ README.markdown
@@ -0,0 +1,3 @@
+# Ruby on Rails Tutorial App
+
+This is a run through of the tutorial at [http://railstutorial.org](http://railstutorial.org) by [Michael Hartl](http://michaelhartl.com).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TutorialApp::Application.config.secret_token = 'cac985102edc4f674b540d43d16e3e16e23e8e17e3ddb2362fb2eabbd0311cb8b9b6482e07fce76ad9c0ab67ab4a3d8e2f337d08c7f44fdc6290e076cf6e7b1f'
